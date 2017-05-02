4043dcaa9386a767742cc788eded6dd2869e5b00
Moutaz <Moutaz@MOUTAZ-PC>
2017-05-02 10:59:36 -0500

    FIxed typos.

--- src/mcmc.f90
+++ src/mcmc.f90
@@ -14,11 +14,11 @@ interface
   function lnlikelihood(c, r200, a, b, phi, theta) bind(c, name="triaxUTD_lnlikelihood")
 	real(c_double) :: lnlikelihood
 	real(c_double), value :: c
-	real(c_double), value  :: r200
-	real(c_double), value  :: a
-	real(c_double), value  :: b
-	real(c_double), value  :: phi
-	real(c_double), value  :: theta
+	real(c_double), value :: r200
+	real(c_double), value :: a
+	real(c_double), value :: b
+	real(c_double), value :: phi
+	real(c_double), value :: theta
   end function
 
 end interface
